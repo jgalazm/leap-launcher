20feb5cc4cc4677a22c7e1c00059a5baabcdd35a
José  Galaz <jdgalazm@gmail.com>
2020-04-04 15:53:34 -0300

    endpoit /processes

--- src/main.py
+++ src/main.py
@@ -1,7 +1,28 @@
-from quart import Quart
+import os
+import paramiko
+from quart import Quart, jsonify
+import launcher
+
+key = paramiko.RSAKey.from_private_key_file("./launcher/id_rsa")
+username = os.environ['SUDO_USER']
+sudo_password = os.environ['SUDO_PASS']
+cmd_runner = launcher.CommandRunner(username, key, sudo_password)
+
 
 app = Quart(__name__)
+@app.route('/processes', methods=['GET'])
+async def list_processes():
+    processes = cmd_runner.get_listening_process_list()
+    return jsonify({
+        "processes": processes 
+    })
 
-@app.route('/')
-async def hello():
-    return 'hello'
+
+# /kill
+
+# /launch/web
+
+# /launch/hands
+
+# /launch/leapd
+
