b8076519a4ba680d8c1b9c7b9b5f528b3fa0d261
José  Galaz <jdgalazm@gmail.com>
2020-04-12 17:33:12 -0400

    get processes list with interval every 3s

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,58 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
-import styles from './App.module.css';
+import React from "react";
+import logo from "./logo.svg";
+import styles from "./App.module.css";
+import * as Utils from './Utils';
 
-const getList = () => {
-  return fetch("http://localhost:5000/api/processes")
-    .then((r) => r.json())
-    .then((r) => {
-      const element = document.getElementById("logs");
-      element.innerText = JSON.stringify(r, null, 2);
-    });
-};
-const killAll = () => {
-  return fetch("http://localhost:5000/api/kill", { method: "DELETE" })
-    .then((r) => r.json())
-    .then((r) => {
-      console.log("api/kill", r);
-    });
-};
-
-const webServer = () => {
-  return fetch("http://localhost:5000/api/launch/web", { method: "POST" }).then(
-    (r) => {
-      console.log("api/launch/web", r);
-    }
-  );
-};
-
-const handsServer = () => {
-  return fetch("http://localhost:5000/api/launch/hands", { method: "POST" }).then(
-    (r) => {
-      console.log("api/launch/hands", r);
-    }
-  );
-};
-
-const leapServer = () => {
-  return fetch("http://localhost:5000/api/launch/leapd", { method: "POST" }).then(
-    (r) => {
-      console.log("api/launch/leapd", r);
-    }
-  );
-};
-
-const KILLING = 'KILLING';
-const LAUNCHING = 'LAUNCHING';
-const READY = 'READY';
-
+const STARTING = "STARTING";
+const KILLING = "KILLING";
+const LAUNCHING = "LAUNCHING";
+const READY = "READY";
 
 function App() {
-  return (
-    <div className={styles.App}>
-        asd
-    </div>
-  );
+  const [screen, setScreen] = React.useState(STARTING);
+  const [processesList, setProcessesList] = React.useState([]);
+  React.useEffect(() => {
+    const interval = setInterval(() => {
+      Utils.getList().then(async r => {
+        if (r.ok) {
+          const list = await r.json()
+          setProcessesList(list['processes']);
+        }
+      })
+      return () => {
+        clearInterval(interval);
+      }
+    }, 3000);
+
+  }, [])
+
+  return <div className={styles.App}>{processesList.map(r => <div key={r}>{r}</div>)}</div>;
 }
 
 export default App;
